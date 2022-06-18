43c75f376024aec684a07ed3b68b1c32908ab604
Maureen0007 <maureen.wanjiku@student.moringaschool.com>
2022-06-19 01:01:08 +0300

    complete assignment

---
+++ js/challenge.js
@@ -0,0 +1,18 @@
+const counter = document.getElementById("counter")
+counter.addEventListener("DOMContentLoaded", timerIncrement)
+
+function timerIncrement(){
+
+    let count = 0
+    if(counter >=0 ){
+    return count++;
+    }
+}
+
+timerIncrement()
+
+
+let minus = document.getElementById("minus")
+let plus = document.getElementById("plus")
+let pause = document.getElementById("pause")
+let  heart = document.getElementById("heart") 
